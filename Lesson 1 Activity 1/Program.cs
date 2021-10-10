3b00722cd40d9099b435d18a689caf1d795b6176
tashmcphee <42576134+tashmcphee@users.noreply.github.com>
2021-10-10 21:13:04 +1000

    Lesson 1 Activity 1
    First Commit

---
+++ Lesson 1 Activity 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lesson_1_Activity_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+            Console.WriteLine("And so we've started on our road");
+            Console.WriteLine("Of learning C# as we learn to code");
+        }
+        }
+    }
+}
