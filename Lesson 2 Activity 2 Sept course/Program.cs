aba7b2f4e2303c93b3b635ea273c28798223730a
tashmcphee <42576134+tashmcphee@users.noreply.github.com>
2021-10-10 21:06:58 +1000

    Lesson 2 Activity 2
    First commit

---
+++ Lesson 2 Activity 2 Sept course/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson_2_Activity_2_Sept_course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
