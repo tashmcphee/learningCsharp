f2dd613787e54fb7ceee415966fe67abd934bdbe
tashmcphee <42576134+tashmcphee@users.noreply.github.com>
2021-10-10 21:24:10 +1000

    Lesson 2 Activity 2
    First Commit

---
+++ Lesson 2 Activity 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lesson_2_Activity_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        var questionone = ("Favourite Colour");
+            
+    Console.WriteLine(questionone);
+    var question1 = Console.ReadLine();
+
+            var questiontwo = ("mermaid or unicorn?");
+
+            Console.WriteLine(questiontwo);
+            var question2 = Console.ReadLine();
+
+            var questionthree = ("Favourite song");
+            Console.WriteLine(questionthree);
+            var question3 = Console.ReadLine();
+
+            Console.WriteLine($"I like {question1} {question2} that sing {question3} !");
+        }
+    }
+}
