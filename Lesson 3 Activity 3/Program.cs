98ee9bf441e744028017b9e922a64f5bfc19f172
tashmcphee <42576134+tashmcphee@users.noreply.github.com>
2021-10-10 21:07:40 +1000

    Lesson 3 Activity 3
    First Commit

---
+++ Lesson 3 Activity 3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson_3_Activity_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var deposit = "What is your Inital deposit?";
+            Console.WriteLine(deposit);
+            var a = Console.ReadLine();
+
+            var interestrate = "What interest rate";
+            Console.WriteLine(interestrate);
+            var b = Console.ReadLine();
+
+            var months = "How long will you be saving for?";
+
+            Console.WriteLine(months);
+            var c = Console.ReadLine();
+
+            //var totalsavings = Math.Pow(a,b);
+            //Console.WriteLine(totalsavings);
+
+            //need to finish
+        }
+    }
+}
